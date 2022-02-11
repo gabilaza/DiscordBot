d8688334aaa8a80bd8526139b16ff53dcceb1366
Gabriel Laza <gabriellaza02@gmail.com>
2022-02-11 14:53:22 +0200

    Add different playlist for every server

--- domain/music.py
+++ domain/music.py
@@ -51,18 +51,14 @@ class YTDLSource(discord.PCMVolumeTransformer):
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
-class Music(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+class Playlist:
+    def __init__(self):
         self.queue = []
         self.loop = "None"
         self.indexQ = 0
 
 
-    @commands.command()
-    @commands.guild_only()
-    async def repeat(self, ctx, option=''):
-        """Repeat songs (None or ->, Repeat1 or 1, RepeatQueue or l)"""
+    def repeat(self, option):
         if option == '':
             if self.loop == "None":
                 self.loop = "Repeat1"
@@ -70,45 +66,110 @@ class Music(commands.Cog):
                 self.loop = "RepeatQueue"
             elif self.loop == "RepeatQueue":
                 self.loop = "None"
-            await ctx.send(f'```Repeat: {self.loop}```')
+            return f'```Repeat: {self.loop}```'
         else:
             if option == 'None' or option == '->':
                 self.loop = 'None'
-                await ctx.send(f'```Repeat: {self.loop}```')
+                return f'```Repeat: {self.loop}```'
             elif option == 'Repeat1' or option == '1':
                 self.loop = 'Repeat1'
-                await ctx.send(f'```Repeat: {self.loop}```')
+                return f'```Repeat: {self.loop}```'
             elif option == 'RepeatQueue' or option == 'l':
                 self.loop = 'RepeatQueue'
-                await ctx.send(f'```Repeat: {self.loop}```')
+                return f'```Repeat: {self.loop}```'
             else:
-                await ctx.send('```The option is not correct```')
+                return '```The option is not correct```'
+
+
+    def add(self, url):
+        self.queue.append(url)
+
+
+    def show(self):
+        if self.queue:
+            return f'```Queue:\n - '+'\n - '.join(self.queue)+'```'
+        else:
+            return '```Queue is empty```'
+
+
+    def clear(self):
+        self.queue = []
+        self.indexQ = 0
+        return '```Queue cleared```'
+
+
+    def next_song(self):
+        if self.loop == "None":
+            del(self.queue[self.indexQ])
+            if self.indexQ != 0:
+                self.indexQ = 0
+        elif self.loop == "Repeat1":
+            pass
+        elif self.loop == "RepeatQueue":
+            self.indexQ += 1
+            if self.indexQ >= len(self.queue):
+                self.indexQ = 0
+
+
+    def get_song(self):
+        if self.queue:
+            return self.queue[self.indexQ]
+        return None
+
+
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.queue = []
+        self.loop = "None"
+        self.indexQ = 0
+        self.playlists = {}
+
+
+    def get_playlist(self, ctx):
+        try:
+            playlist = self.playlists[ctx.guild.id]
+        except:
+            playlist = Playlist()
+            self.playlists[ctx.guild.id] = playlist
+
+        return playlist
+
+
+    @commands.command()
+    @commands.guild_only()
+    async def repeat(self, ctx, option=''):
+        """Repeat songs (None or ->, Repeat1 or 1, RepeatQueue or l)"""
+        playlist = self.get_playlist(ctx)
+        s = playlist.repeat(option)
+        await ctx.send(s)
 
 
     @commands.command()
     @commands.guild_only()
     async def qadd(self, ctx, *, url):
         """Add song"""
-        self.queue.append(url)
+        playlist = self.get_playlist(ctx)
+        playlist.add(url)
 
 
     @commands.command()
     @commands.guild_only()
     async def qshow(self, ctx):
         """Show songs"""
-        if self.queue:
-            await ctx.send(f'```Queue:\n - '+'\n - '.join(self.queue)+'```')
-        else:
-            await ctx.send('```Queue is empty```')
+        playlist = self.get_playlist(ctx)
+        s = playlist.show()
+        await ctx.send(s)
 
 
     @commands.command()
     @commands.guild_only()
     async def qclear(self, ctx):
         """Clear songs"""
-        self.queue = []
-        self.indexQ = 0
-        await ctx.send('```Queue cleared```')
+        playlist = self.get_playlist(ctx)
+        s = playlist.clear()
+        await ctx.send(s)
 
 
     @commands.command()
@@ -122,18 +183,8 @@ class Music(commands.Cog):
 
 
     async def play_next(self, ctx):
-        if self.loop == "None":
-            del(self.queue[self.indexQ])
-            if self.indexQ != 0:
-                self.indexQ = 0
-        elif self.loop == "Repeat1":
-            pass
-        elif self.loop == "RepeatQueue":
-            self.indexQ += 1
-            if self.indexQ >= len(self.queue):
-                self.indexQ = 0
-
-        if self.queue:
+        playlist = self.get_playlist(ctx)
+        if playlist.get_song():
             await self.play(ctx)
 
 
@@ -141,14 +192,15 @@ class Music(commands.Cog):
     @commands.guild_only()
     async def play(self, ctx):
         """Play the queue"""
-        if self.queue:
+        playlist = self.get_playlist(ctx)
+        if playlist.get_song():
             def handleNext(error, ctx):
                 if error:
                     print('Player error: %s' % error)
                 else:
                     asyncio.run_coroutine_threadsafe(self.play_next(ctx), self.bot.loop)
 
-            player = await YTDLSource.from_url(self.queue[self.indexQ], loop=self.bot.loop, stream=True)
+            player = await YTDLSource.from_url(playlist.get_song(), loop=self.bot.loop, stream=True)
             ctx.voice_client.play(player, after=lambda e: handleNext(e, ctx))
             await ctx.send(f'```Now playing: {player.title}```')
         else:
@@ -166,9 +218,7 @@ class Music(commands.Cog):
     @commands.guild_only()
     async def leave(self, ctx):
         """Disconnects the bot from voice"""
-        self.queue = []
-        self.indexQ = 0
-        self.loop = 'None'
+        del(self.playlists[ctx.guild.id])
         await ctx.voice_client.disconnect()
 
 
